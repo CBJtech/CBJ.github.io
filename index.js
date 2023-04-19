c895afd9f504a47654d297b7f8be8a61b946ba33
CBJtech <130850828+CBJtech@users.noreply.github.com>
2023-04-19 15:40:07 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let first = document.getElementById('bar_btn');
+let last = document.getElementById('start');
+let middle =document.getElementById('linkks');
+
+middle.onclick = function(){
+    var bar = document.getElementById("navbar");
+    bar.style.display = 'none';
+    }
+   
+first.onclick = function(){
+ var bar = document.getElementById("navbar");
+ bar.style.display = 'block';
+ }
+ last.onclick = function(){
+ var bar = document.getElementById("navbar");
+ bar.style.display = 'block';
+ }
+ 
